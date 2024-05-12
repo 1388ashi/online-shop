0b3cd6d4fbbcccbe2cf1df512f2ca543a9aedbf3
1388ashi <arshiabat5@gmail.com>
2024-05-12 12:43:02 +0430

    first commit

--- Modules/Area/Providers/RouteServiceProvider.php
+++ Modules/Area/Providers/RouteServiceProvider.php
@@ -49,7 +49,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->moduleNamespace)
-            ->group(module_path('Area', '/Routes/web.php'));
+            ->group(module_path('Area', '/routes/web.php'));
     }
 
     /**
