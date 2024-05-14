004e0f08f23f525f8cb6f55d5fe73ddf461049e0
1388ashi <arshiabat5@gmail.com>
2024-05-14 17:04:18 +0430

    dashboard providers

--- Modules/Auth/app/Http/Requests/Customer/VerifyRequest.php
+++ Modules/Auth/app/Http/Requests/Customer/VerifyRequest.php
@@ -29,9 +29,10 @@ class VerifyRequest extends FormRequest
             throw Helpers::makeValidationException('کاربری با این شماره موبایل پیدا نشد!', 'mobile');
         } elseif ($smsToken->token !== $this->sms_token) {
             throw Helpers::makeValidationException('کد وارد شده نادرست است!', 'sms_token');
-        } elseif (Carbon::now()->gt($smsToken->expires_at)) {
-            throw Helpers::makeValidationException('کد وارد شده منقضی شده است!', 'sms_token');
-        }
+        } 
+        // elseif (Carbon::now()->gt($smsToken->expires_at)) {
+        //     throw Helpers::makeValidationException('کد وارد شده منقضی شده است!', 'sms_token');
+        // }
 
         if ($this->input('type') !== 'register') {
             $customer = Customer::where('mobile', $this->input('mobile'))->first();
