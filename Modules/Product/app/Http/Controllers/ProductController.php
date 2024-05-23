fb83deef458f5859994739c021edc17991a8b2fe
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-05-23 13:18:11 +0330

    Update ProductController.php

--- Modules/Product/app/Http/Controllers/ProductController.php
+++ Modules/Product/app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
         $categories = Category::query()->whereNull('parent_id')->select(['id','name'])->get();
 
         $products = Product::query()
-        ->with(['category:id,name','specificaion:id,title'])
+        ->with(['category:id,name','specificaions:id,title'])
         ->latest('id')
         ->paginate();
 
