afb3a27774daf429e721f5548dd4bcda2e4be2f1
1388ashi <143864702+1388ashi@users.noreply.github.com>
2024-05-26 09:22:36 +0330

    Update UpdateRequest.php

--- Modules/Customer/app/Http/Requests/UpdateRequest.php
+++ Modules/Customer/app/Http/Requests/UpdateRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Modules\Core\App\Rules\IranMobile;
 
-class updateRequest extends FormRequest
+class UpdateRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
